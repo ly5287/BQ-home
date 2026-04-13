99037455d39277cdd50e6393551b265509a5bd6c
Backup User <backup@example.com>
2026-04-13 17:57:48 +0800

    改了本地搜索页的形式

--- start.bat
+++ start.bat
@@ -2,8 +2,6 @@
 cd /d F:\bq\_website\mywebsite
 hugo --baseURL http://localhost:3001/
 .\pagefind_extended.exe --site public
-echo 替换搜索页面...
-powershell -ExecutionPolicy Bypass -File replace-search.ps1
 cd public
 http-server -p 3001 -c-1
 pause
