24df0bb5cfea5a36836bf83b0045ad1064134cbf
Backup User <backup@example.com>
2026-06-19 14:23:55 +0800

    update 2026/06/19/周五 14:23:47.23

---
+++ themes/hugo-theme-relearn/public/searchindex.en.js
@@ -0,0 +1,18 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "${{ steps.pages.outputs.base_url }}/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "${{ steps.pages.outputs.base_url }}/tags/index.html"
+  }
+]
